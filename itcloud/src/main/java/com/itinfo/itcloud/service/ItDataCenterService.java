5ae920d862213cb066dd2ec215bcbad9f2a94f3e
deh22 <deh2246@naver.com>
2024-03-15 17:03:54 +0900

    [리팩토링] datacenter

--- itcloud/src/main/java/com/itinfo/itcloud/service/ItDataCenterService.java
+++ itcloud/src/main/java/com/itinfo/itcloud/service/ItDataCenterService.java
@@ -12,10 +12,6 @@ import java.util.List;
 public interface ItDataCenterService {
     String getName(String id);
     List<DataCenterVo> getList();
-//    List<DomainVo> getStorage(String id);
-//    List<NetworkVo> getNetwork(String id);
-//    List<ClusterVo> getCluster(String id);
-//    List<PermissionVo> getPermission(String id);
     List<EventVo> getEvent(String id);
 
 
@@ -24,5 +20,12 @@ public interface ItDataCenterService {
     CommonVo<Boolean> addDatacenter(DataCenterCreateVo dcVo);   // 생성
     CommonVo<Boolean> editDatacenter(DataCenterCreateVo dcVo);   // 수정
     CommonVo<Boolean> deleteDatacenter(String id);       // 삭제
+
+    // region: 안쓸 것 같음
+//    List<DomainVo> getStorage(String id);
+//    List<NetworkVo> getNetwork(String id);
+//    List<ClusterVo> getCluster(String id);
+//    List<PermissionVo> getPermission(String id);
+    // endregion
 
 }
