1aee27b3833d94de49c62b4997b93fa341ea3e8e
deh22 <deh2246@naver.com>
2024-06-25 10:30:32 +0900

    [fix]dashboard5

--- itcloud/src/main/java/com/itinfo/itcloud/controller/DashboardController.java
+++ itcloud/src/main/java/com/itinfo/itcloud/controller/DashboardController.java
@@ -1,16 +1,19 @@
 package com.itinfo.itcloud.controller;
 
-import com.itinfo.itcloud.service.ItDashboardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class DashboardController {
-	private final ItDashboardService dashboardService;
 
+	@GetMapping("/dash")
+	public String dashboard(){
+		return "dash/Darkdashboard5";
+	}
 
 }
 
