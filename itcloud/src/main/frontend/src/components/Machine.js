ac82b435df31a0ed420e2da9e72d320cd545a743
minyoung <lmy9237@naver.com>
2024-07-14 16:11:38 +0900

    [add] 팝업창추가

--- itcloud/src/main/frontend/src/components/Machine.js
+++ itcloud/src/main/frontend/src/components/Machine.js
@@ -713,6 +713,8 @@ const SnapshotSection = () => {
 
 // 애플리케이션
 const ApplicationSection = () => {
+
+
   return (
     <div id="application_outer">
       <div id="application_content">
@@ -744,11 +746,17 @@ const ApplicationSection = () => {
 
 // 선호도그룹
 const PregroupSection = () => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // 팝업 열기/닫기 핸들러
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <div id="pregroup_outer">
       <div className="pregroup_content">
         <div className="content_header_right">
-          <button id="pregroup_create_btn">새로 만들기</button>
+          <button id="pregroup_create_btn" onClick={openModal}>새로 만들기</button>
           <button>편집</button>
           <button>제거</button>
         </div>
@@ -812,6 +820,113 @@ const PregroupSection = () => {
           </tbody>
         </table>
       </div>
+
+      {/*네트워크 인터페이스(새로만들기) */}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="새로만들기"
+        className="network_popup"
+        overlayClassName="network_popup_outer"
+      >
+                    <div className="network_popup_header">
+                <h1>네트워크 인터페이스 수정</h1>
+                <button  onClick={closeModal}><i className="fa fa-times"></i></button>
+            </div>
+
+            <div className="network_popup_content">
+                <div className="input_box">
+                    <span>이름</span>
+                    <input type="text"/>
+                </div>
+                <div className="select_box">
+                    <label htmlFor="profile">프로파일</label>
+                    <select id="profile">
+                        <option value="test02">ovirtmgmt/ovirtmgmt</option>
+                    </select>
+                </div>
+                <div className="select_box">
+                    <label htmlFor="type" style={{ color: 'gray' }}>유형</label>
+                    <select id="type" disabled>
+                        <option value="test02">VirtIO</option>
+                    </select>
+                </div>
+                <div className="select_box2" style={{ marginBottom: '2%' }}>
+                    <div>
+                        <input type="checkbox" id="custom_mac_box" disabled/>
+                        <label htmlFor="custom_mac_box" style={{ color: 'gray' }}>
+                            사용자 지정 MAC 주소
+                        </label>
+                    </div>
+                    <div>
+                        <select id="mac_address" disabled>
+                            <option value="test02">VirtIO</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className="plug_radio_btn">
+                    <span>링크 상태</span>
+                    <div>
+                        <div className="radio_outer">
+                            <div>
+                                <input type="radio" name="status" id="status_up"/>
+                                <img src=".//img/스크린샷 2024-05-24 150455.png" alt="status_up"/>
+                                <label htmlFor="status_up">Up</label>
+                            </div>
+                            <div>
+                                <input type="radio" name="status" id="status_down"/>
+                                <img src=".//img/Down.png" alt="status_down"/>
+                                <label htmlFor="status_down">Down</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="plug_radio_btn">
+                    <span>카드 상태</span>
+                    <div>
+                        <div className="radio_outer">
+                            <div>
+                                <input type="radio" name="connection_status" id="connected"/>
+                                <img src=".//img/연결됨.png" alt="connected"/>
+                                <label htmlFor="connected">연결됨</label>
+                            </div>
+                            <div>
+                                <input type="radio" name="connection_status" id="disconnected"/>
+                                <img src=".//img/분리.png" alt="disconnected"/>
+                                <label htmlFor="disconnected">분리</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="network_parameter_outer">
+                <span>네트워크 필터 매개변수</span>
+                <div>
+                    <div>
+                        <span>이름</span>
+                        <input type="text"/>
+                    </div>
+                    <div>
+                        <span>값</span>
+                        <input type="text"/>
+                    </div>
+                    <div id="buttons">
+                        <button>+</button>
+                        <button>-</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="edit_footer">
+                <button style={{ display: 'none' }}></button>
+                <button>OK</button>
+                <button  onClick={closeModal}>취소</button>
+            </div>
+      </Modal>
+
+
     </div>
   );
 };
